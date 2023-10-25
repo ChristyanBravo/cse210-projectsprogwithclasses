25f59181f17b9f1404748e0551c3beee63706e33
ChristyanBravo <christyanbravosilva@gmail.com>
2023-10-25 00:52:37 -0500

    WEEK 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,53 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); 
+        int guess;
+        int attempts = 0;
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Console.WriteLine("Welcome to Guess My Number Game!");
+            Console.WriteLine("I have selected a magic number between 1 and 100.");
+            Console.WriteLine("Can you guess it?");
+
+            do
+            {
+                Console.Write("Enter your guess: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                attempts++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {attempts} attempts!");
+                }
+            } while (guess != magicNumber);
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgainInput = Console.ReadLine().ToLower();
+
+            if (playAgainInput != "yes")
+            {
+                playAgain = false;
+                Console.WriteLine("Thank you for playing. Goodbye!");
+            }
+            else
+            {
+                magicNumber = random.Next(1, 101); 
+                attempts = 0; 
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        
+        double average = (double)sum / numbers.Count;
+
+        // Find maximum number
+        int maxNumber = numbers.Count > 0 ? numbers[0] : 0;
+        foreach (int num in numbers)
+        {
+            if (num > maxNumber)
+            {
+                maxNumber = num;
+            }
+        }
+
+        // Display results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
